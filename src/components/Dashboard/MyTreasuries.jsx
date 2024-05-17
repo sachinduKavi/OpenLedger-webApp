e4837c98d4ba9dbeb0bcf793ef731b1d9413c14a
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-05-17 07:16:25 +0530

    treasury

--- src/components/Dashboard/MyTreasuries.jsx
+++ src/components/Dashboard/MyTreasuries.jsx
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from 'react'
 
+import {getAllTreasuryParticipantData} from '../../query/treasuryQuery'
+
 import '../../styles/my-treasuries.css'
 import TreasuryGig from './TreasuryGig'
 
 
 
-export default function MyTreasuries() {
+export default function MyTreasuries(props) {
 
   // Component did mount ?
   useEffect(() => {
-    
+
+    // await getAllTreasuryParticipantData()
   })
 
   return (
