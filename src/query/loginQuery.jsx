165f04cfe7927d693d903c1c120f27ec23431859
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-05-22 12:45:54 +0530

    token

--- src/query/loginQuery.jsx
+++ src/query/loginQuery.jsx
@@ -4,7 +4,7 @@ const cDomain = temp_domain
 
 // Call API to check whether the user credentials are correct
 const userLogin = async (user) => {
-    const res = await cDomain.post('user/checkLogin', user, header)
+    const res = await cDomain.post('user/checkLogin', {...user, setHeader: true}, header)
     return res.data
 }
 
