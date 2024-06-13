d144a1c43d7c9fda2de95d2dd0a3b9ea236fc539
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-06-13 07:51:14 +0530

    participants

--- src/components/Treaasury/SingleParticipant.jsx
+++ src/components/Treaasury/SingleParticipant.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 import SimpleDP from '../SimpleDP'
 import CardIcon from '../../assets/icons/card.png'
 
-export default function SingleParticipant() {
+export default function SingleParticipant(props) {
   return (
     <div className='single-participant-border'>
-        <SimpleDP imageLink='https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'
-            imageScale={{x:0, y:0, scale: 1}} size={50}
-        />
+
+        <div className="counter">
+          <h2>1</h2>
+          <SimpleDP imageLink='https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'
+              imageScale={{x:0, y:0, scale: 1}} size={50}
+          />
+        </div>
+
+        
 
         <div className="user-details">
             <h4>Prathibha Dulakshani</h4>
@@ -19,9 +25,16 @@ export default function SingleParticipant() {
             <h4>sachindu38@gmail.com</h4>
         </div>
 
-        <h4>0764314505</h4>
+        <div className="mobile-num">
+          <h4>0764314505</h4>
+        </div>
+        
 
-        <h3>Member</h3>
+        <div className="position">
+          <h3>Member</h3>
+        </div>
+
+        
 
         <img src={CardIcon} alt="card-icon" />
     </div>
