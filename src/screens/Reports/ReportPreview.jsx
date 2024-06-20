a6af334dc130bcb8ea5dbfa5c82fb2433ab0bf00
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-06-21 03:27:22 +0530

    report

---
+++ src/screens/Reports/ReportPreview.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import '../../styles/report-preview.css'
+
+export default function ReportPreview() {
+  return (
+    <div className='report-preview-border'>
+        <div className="row">
+            <h2>REPORT PREVIEW</h2>
+
+            <button style={{backgroundColor: '#32AF4E'}}>NEW</button>
+        </div>
+
+        
+        
+    </div>
+  )
+}
